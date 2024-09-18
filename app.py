96be0553c3e9c6c4523199d38ca2a69919799cba
Alexandre Chanson <a.chanson.tours@gmail.com>
2024-09-18 14:25:12 +0200

    Code for lab

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    input_ = request.args.get('input', default=1, type=int)
+    print("Received query:", request.url)
+    data = compute_heavy_function(input_)
+    return render_template('index.html', fibo=data)
+
+def compute_heavy_function(n):
+    if n == 0:
+        return 1
+    if n == 1:
+        return 1
+    else:
+        return compute_heavy_function(n-1) + compute_heavy_function(n-2)
+
 
 if __name__ == '__main__':
     app.run()
